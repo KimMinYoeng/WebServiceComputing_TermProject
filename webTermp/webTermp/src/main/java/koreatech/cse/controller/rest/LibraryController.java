95ce9be80bffd2c0dc2e7e907b5ed350dc607434
JOSEUNGHO\JO.Sh <hellowkc12@koreatech.ac.kr>
2017-12-07 17:43:34 +0900

    로그인에 따라 서비스 이용가능 하게

---
+++ webTermp/webTermp/src/main/java/koreatech/cse/controller/rest/LibraryController.java
@@ -0,0 +1,63 @@
+package koreatech.cse.controller.rest;
+
+import koreatech.cse.service.EducationService;
+import koreatech.cse.service.EventService;
+import koreatech.cse.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/service/library")
+public class LibraryController {
+    @Autowired
+    private LibraryService libraryService;
+    @Autowired
+    private EventService eventService;
+    @Autowired
+    private EducationService educationService;
+
+    //키워드가 있을때도 있고 없을때도있음
+    //있을때는 가져가고 없을때는 안가져가고
+    @RequestMapping("/list.do")
+    public ModelAndView list(@RequestParam(required=false)String district){
+        ModelAndView mav = new ModelAndView();
+        if(district != "")
+        {
+            mav.addObject("library_list",libraryService.serachLibrary(district));
+        } else
+            mav.addObject("library_list", null);
+
+        mav.setViewName("library_list");
+
+        return mav;
+    }
+
+    @RequestMapping("/event.do")
+    public ModelAndView schedule(@RequestParam(required=false)String district_event, @RequestParam(required=false)String library_event){
+        ModelAndView mav = new ModelAndView();
+        if((district_event != ""  && library_event != "") || (district_event != "" && library_event == ""))
+            mav.addObject("library_event",eventService.serachEvent(district_event, library_event));
+        else
+            mav.addObject("library_event", null);
+
+        mav.setViewName("library_event");
+
+        return mav;
+    }
+
+    @RequestMapping("/education.do")
+    public ModelAndView education(@RequestParam(required=false)String district_service, @RequestParam(required=false)String library_service){
+        ModelAndView mav = new ModelAndView();
+        if((district_service != ""  && library_service != "") || (district_service != "" && library_service == ""))
+            mav.addObject("library_education", educationService.serachEducation(district_service, library_service));
+        else
+            mav.addObject("library_education", null);
+
+        mav.setViewName("library_education");
+
+        return mav;
+    }
+}
---
+++ webTermp/webTermp/src/main/java/koreatech/cse/service/EventService.java
@@ -0,0 +1,114 @@
+package koreatech.cse.service;
+
+import koreatech.cse.domain.rest.Event;
+import org.springframework.stereotype.Service;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+
+@Service
+public class EventService {
+
+    public ArrayList<Event> serachEvent(String district, String library) {
+        ArrayList<Event> list = null;
+        try {
+            URL url = new URL(getURLParam(district, library));
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+            XmlPullParser xpp = factory.newPullParser();
+            BufferedInputStream bis = new BufferedInputStream(url.openStream());
+            xpp.setInput(bis, "utf-8");
+
+            int event_type = xpp.getEventType();
+
+            String addr = null;
+            Event event = null;
+
+            while (event_type != XmlPullParser.END_DOCUMENT) {
+                switch (event_type) {
+                    case XmlPullParser.END_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_DOCUMENT:
+                        list = new ArrayList<Event>();
+                        break;
+                    case XmlPullParser.END_TAG: {
+                        String tag = xpp.getName();
+                        if (tag.equals("row")) {
+                            list.add(event);
+                            event = null;
+                        }
+                    }
+
+                    case XmlPullParser.START_TAG: {
+                        String tag = xpp.getName();
+                        if (tag.equals("row")) {
+                            event = new Event();
+
+                        } else if (tag.equals("EVENT_NM")) {
+                            if (event != null)
+                                event.setName(xpp.nextText());
+                        } else if (tag.equals("LBRRY_NAME")) {
+                            if (event != null)
+                                event.setLibrary(xpp.nextText());
+                        } else if (tag.equals("EVENT_BGNDE")) {
+                            if (event != null)
+                                event.setBegindate(xpp.nextText());
+                        } else if (tag.equals("EVENT_ENDDE")) {
+                            if (event != null)
+                                event.setEnddate(xpp.nextText());
+                        } else if (tag.equals("TRGET")) {
+                            if (event != null)
+                                event.setTarget(xpp.nextText());
+                        } else if (tag.equals("PLACE")) {
+                            if (event != null)
+                                event.setPlace(xpp.nextText());
+                        }
+                        break;
+                    }
+                }
+                event_type = xpp.next();
+            }
+
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private String getURLParam(String search, String search2){
+        String url = "";
+        System.out.println(search);
+
+        try{
+            String district = URLEncoder.encode(search, "UTF-8");
+            String library = URLEncoder.encode(search2, "UTF-8");
+            System.out.println(district);
+            url = "http://openapi.seoul.go.kr:8088/7367785455676f6c3535724f7a5a4c/xml/SeoulLibraryEvent/1/30/" + district + "/" + library;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+}
